215aa968f3785b57bde801256b12ef04bc3e5e7b
Asheni Perera <Ashenika@users.noreply.github.com>
2022-08-17 13:15:07 +0530

    Add files via upload

---
+++ template_cs/Program.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using template_cs;
+
+public class Program
+{
+    private static void Main(string[] args)
+    {
+        int result = Calculator.Add(1, 2);
+        Console.WriteLine("Add two numbers: 1 + 2 = " + result);
+    }
+}
